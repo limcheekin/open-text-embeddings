e9d96bb3a6fcd89d93f6d083dc97a53396b77bfd
Lim Chee Kin <limcheekin@vobject.com>
2023-08-08 12:12:09 +0800

    chore: download all files of the model

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@ mkdir -p $MODEL
 git lfs install --skip-smudge
 git clone https://huggingface.co/$MODEL $MODEL
 cd $MODEL
-git lfs pull --include "*.model"
+git lfs pull
 git lfs install --force
 rm -rf .git
 pwd
